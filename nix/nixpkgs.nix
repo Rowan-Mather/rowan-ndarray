bb795bf6a9ae8a38453cfe64487194972c106cec
Aiken Cairncross <acairncross@gmail.com>
2023-07-12 12:46:52 +0100

    Remove unneeded things from nixpkgs overlay

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,20 +1,18 @@
 { sources ? import ./sources.nix }:
 
-let clash-compiler = import sources.clash-compiler {};
-
-    overlay = _: pkgs: {
-        niv = (import sources.niv {}).niv;
-        haskellPackages = pkgs.haskellPackages.override {
-          overrides = self: super: {
-            dense = pkgs.haskell.lib.markUnbroken (pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.overrideSrc super.dense {
-              src = sources.dense;
-            }));
-            yoda =
-              pkgs.haskell.lib.doJailbreak
-                (self.callCabal2nix "yoda" sources.yoda {});
-          } // clash-compiler;
-        };
-      };
+let overlay = _: pkgs: {
+  niv = (import sources.niv {}).niv;
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: {
+      dense = pkgs.haskell.lib.markUnbroken (
+        pkgs.haskell.lib.dontCheck ( # doctests are broken
+          pkgs.haskell.lib.overrideSrc super.dense {
+            src = sources.dense;
+          }
+        )
+      );
+    };
+  };
+};
 in
-import sources.nixpkgs
-  { overlays = [ overlay ] ; config = { }; }
+import sources.nixpkgs { overlays = [ overlay ] ; config = {}; }
