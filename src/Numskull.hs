c17388e03bcea024373081b49bda1430b3adf91d
Rowan <rowan.m.mather@gmail.com>
2023-07-12 13:36:28 +0100

    first build

--- src/Numskull.hs
+++ src/Numskull.hs
@@ -6,12 +6,15 @@
 module Numskull where
 
 import Prelude as P
-import Data.Maybe (isJust, fromJust)
+--import Data.Maybe (isJust, fromJust)
 import Data.Dynamic
 import Type.Reflection
 import Data.Vector as V
 
-ty x = typeOf x
+import DType
+
+ty :: Typeable a => a -> TypeRep a
+ty x = typeOf x -- Shorthand
 
 -- NdArray --
 data NdArray where
@@ -23,12 +26,11 @@ instance Show NdArray where
 -- To do: https://hackage.haskell.org/package/base-4.18.0.0/docs/GHC-Num.html
 instance Num NdArray where
     (NdArray x) + (NdArray y) = case (eqTypeRep xtype ytype, matchDType (NdArray x) (NdArray y)) of
-        (Just HRefl, _)         -> NdArray (V.zipWith add x y) -- Types match
-        (_, Just casted)        -> (NdArray x) + casted -- Second type can be converted to first
-        otherwise               -> error ("Cannot convert second matrix of type '" P.++ show ytype P.++ "' to type '" P.++ show xtype P.++ "'.")
+        (Just HRefl, _)     -> NdArray (V.zipWith add x y) -- Types match
+        (_, Just casted)    -> (NdArray x) + casted -- Second type can be converted to first
+        _                   -> error ("Cannot convert second matrix of type '" P.++ show ytype P.++ "' to type '" P.++ show xtype P.++ "'.")
         where
-            xtype = ty x
-            ytype = ty y
+            xtype = ty x; ytype = ty y
 
     --(NdArray x) - (NDArray y) = 
     
@@ -36,22 +38,25 @@ instance Num NdArray where
 
 
 -- Helper 
+eqDType :: (Typeable a, Typeable b) => a -> b -> Bool
 eqDType x y = case eqTypeRep (ty x) (ty y) of 
-    Just HRefl -> True 
-    otherwise -> False 
+    Just HRefl  -> True 
+    _           -> False 
 
 matchDType :: NdArray -> NdArray -> Maybe NdArray
 matchDType (NdArray x) (NdArray y) = case eqTypeRep (ty x) (ty (fromList [1::Int])) of
-    Just HRefl -> Just $ NdArray (V.map dtypeToInt y)
-    otherwise -> Nothing
+    Just HRefl  -> Just $ NdArray (V.map dtypeToInt y)
+    _           -> Nothing
 
 
 ---- Testing
 
+{-
 unwrapND :: NdArray -> (String, Vector Dynamic)
 unwrapND (NdArray x) = case typeOf x of
     vecTypeInt      -> ("Int", V.map toDyn x)
     vecTypeBool     -> ("Bool", V.map toDyn x)
+-}
 
-nd1 = NdArray (fromList [1,2,3::Int])
-nd2 = NdArray (fromList [10,11,12::Int])
+nd1 :: NdArray; nd2 :: NdArray
+nd1 = NdArray (fromList [1,2,3::Int]); nd2 = NdArray (fromList [10,11,12::Int])
