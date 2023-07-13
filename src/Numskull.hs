989698f5a52ae6be6d18d85b43055225d6ffdc12
Rowan <rowan.m.mather@gmail.com>
2023-07-13 17:37:30 +0100

    eq, ord & tidying

--- src/Numskull.hs
+++ src/Numskull.hs
@@ -12,64 +12,85 @@ import Data.Dynamic
 import Type.Reflection
 ---import Data.Vector as V
 import Data.Vector.Storable as V
-
 import DType
 
+-- Typing shorthand
 ty :: Typeable a => a -> TypeRep a
-ty x = typeOf x -- Shorthand
+ty x = typeOf x
 
 -- NdArray --
 data NdArray where
   NdArray :: (Typeable a, DType a, Storable a) => Vector a -> NdArray
 
+-- Todo: show in a nicer shapely form :)
 instance Show NdArray where
   show (NdArray x) = show x 
 
+-- Todo: check shapes are also equal
+instance Eq NdArray where
+  (NdArray x) == (NdArray y) = x == y
+  (NdArray x) /= (NdArray y) = x /= y
+
+-- Todo: should we have some notion of LEQ for a smaller size vector?
+-- Investigate how Vector implements further
+instance Ord NdArray where
+  compare (NdArray x) (NdArray y)   = compare x y
+  (<) (NdArray x) (NdArray y)       = x < y 
+  (<=) (NdArray x) (NdArray y)      = x <= y
+  (>) (NdArray x) (NdArray y)       = x > y
+  (>=) (NdArray x) (NdArray y)      = x >= y
+  max (NdArray x)                   = max x
+  min (NdArray x)                   = min x
+
+-- To do: matrix multiplication :O
 instance Num NdArray where
-    (NdArray x) + (NdArray y) = case (eqTypeRep xtype ytype, matchDType (NdArray x) (NdArray y)) of
-        (Just HRefl, _)     -> NdArray (V.zipWith add x y) -- Types match
-        _                   -> error ("Cannot match second matrix of type '" P.++ show ytype P.++ "' to type '" P.++ show xtype P.++ "'.")
-        where
-            xtype = ty x; ytype = ty y
+  (+) = pointwiseZip add
+  (-) = pointwiseZip subtract
+  (*) = undefined -- Matrix multiplication
+  negate = invert
+  abs = abs
+  signum = signum
+  fromInteger x = NdArray (fromList [x])
 
-    --x - y = pointwiseZip DType.subtract x y 
-    --(NdArray x) * (NDArray y)
-    -- To do: https://hackage.haskell.org/package/base-4.18.0.0/docs/GHC-Num.html
+-- Pointwise Functions
+  -- All the numpy-like functions not defined within the Eq, Ord or Num instances
+  
+  -- Single Argument
+  
+  -- To do ;)
 
-
--- Helper 
-eqDType :: (Typeable a, Typeable b) => a -> b -> Bool
-eqDType x y = case eqTypeRep (ty x) (ty y) of 
-    Just HRefl  -> True 
-    _           -> False 
-
--- case typeOf x `eqTypeRep` typeRep @Integer of NOTE THIS IS INT SYNTAX
-matchDType :: NdArray -> NdArray -> Maybe NdArray
-matchDType (NdArray x) (NdArray y) = case eqTypeRep (ty x) (ty (fromList [1::Int])) of
+  -- Two Arguments
+  pointwiseZip :: (forall t . DType t => t -> t -> t) -> NdArray -> NdArray -> NdArray
+  pointwiseZip zipfunc (NdArray x) (NdArray y) = 
+    case eqTypeRep (ty x) (ty y) of
+      Just HRefl -> NdArray (V.zipWith zipfunc x y) -- Types match
+      Nothing    -> error ("Cannot match second matrix of type '" P.++ show (ty y) P.++ "' to type '" P.++ show (ty x) P.++ "'.")
+  
+  elemMultiply :: NdArray -> NdArray -> NdArray
+  elemMultiply = pointwiseZip multiply
+  
+  elemDivide :: NdArray -> NdArray -> NdArray
+  elemDivide = pointwiseZip divide
+  
+  elemDiv :: NdArray -> NdArray -> NdArray
+  elemDiv = pointwiseZip div
+  
+  elemPower :: NdArray -> NdArray -> NdArray
+  elemPower = pointwiseZip power
+  
+  elemPow :: NdArray -> NdArray -> NdArray
+  elemPow = pointwiseZip pow
+  
+-- Type & Shape Conversion
+  -- Converting between the standard dtypes and changing the shapes of matricies
+  
+  -- To do: add many more possible types you can convert to
+  -- Use the TypeApplications syntax: 
+  -- case typeOf x `eqTypeRep` typeRep @Integer of 
+  matchDType :: NdArray -> NdArray -> Maybe NdArray
+  matchDType (NdArray x) (NdArray y) = case eqTypeRep (ty x) (ty (fromList [1::Int])) of
     Just HRefl  -> Just $ NdArray (V.map dtypeToInt y)
     _           -> Nothing
-
--- Pointwise Operations not defined in Num
-
-{- Pointwise zip with type conversion (TODO)
-pointwiseZip (NdArray x) (NdArray y) = case (eqTypeRep xtype ytype, matchDType (NdArray x) (NdArray y)) of
-        (Just HRefl, _)     -> NdArray (V.zipWith add x y) -- Types match
-        -- Code to auto-cast types
-        --(_, Just casted)    -> (NdArray x) + casted -- Second type can be converted to first
-        _                   -> error ("Cannot match second matrix of type '" P.++ show ytype P.++ "' to type '" P.++ show xtype P.++ "'.")
-        where
-            xtype = ty x; ytype = ty y
--}
-
-pointwiseZip :: (forall t . DType t => t -> t -> t) -> NdArray -> NdArray -> NdArray
-pointwiseZip zipfunc (NdArray x) (NdArray y) = case eqTypeRep (ty x) (ty y) of
-        Just HRefl -> NdArray (V.zipWith zipfunc x y) -- Types match
-        Nothing    -> error ("Cannot match second matrix of type '" P.++ show ytype P.++ "' to type '" P.++ show xtype P.++ "'.")
-        where
-            xtype = ty x; ytype = ty y
-
-elemMultiply x y = pointwiseZip multiply x y
-
 
 
 
@@ -80,6 +101,22 @@ elemMultiply x y = pointwiseZip multiply x y
 
 
 ---- Testing
+
+-- Helper trying to simplify the type checking....
+eqDType :: (Typeable a, Typeable b) => a -> b -> Bool
+eqDType x y = case eqTypeRep (ty x) (ty y) of 
+  Just HRefl  -> True 
+   _           -> False 
+
+{- Pointwise zip with type conversion (TODO)
+pointwiseZip (NdArray x) (NdArray y) = case (eqTypeRep xtype ytype, matchDType (NdArray x) (NdArray y)) of
+        (Just HRefl, _)     -> NdArray (V.zipWith add x y) -- Types match
+        -- Code to auto-cast types
+        --(_, Just casted)    -> (NdArray x) + casted -- Second type can be converted to first
+        _                   -> error ("Cannot match second matrix of type '" P.++ show ytype P.++ "' to type '" P.++ show xtype P.++ "'.")
+        where
+            xtype = ty x; ytype = ty y
+-}
 
 {-
 unwrapND :: NdArray -> (String, Vector Dynamic)
