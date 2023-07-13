74c55200599bc3051178a0abb646747d3664fcf3
Rowan <rowan.m.mather@gmail.com>
2023-07-13 15:23:38 +0100

    123! serialisation!

--- src/Serialisation.hs
+++ src/Serialisation.hs
@@ -22,10 +22,10 @@ getNumpyShape _ = "(3,)" --3x1 explicitly for now
 -- Thanks Chris! https://github.com/cchalmers/dense/blob/6eced9f5a3ab6b5026fe4f7ab4f67a8bce4d6262/src/Data/Dense/Storable.hs#L686
 -- see https://numpy.org/doc/stable/reference/generated/numpy.lib.format.html
 saveNpy :: FilePath -> NdArray -> IO ()
-saveNpy path nd = withBinaryFile path WriteMode $ \h -> do
+saveNpy path (NdArray v) = withBinaryFile path WriteMode $ \h -> do
   let
     -- Unpack vector
-    (NdArray v) = nd
+    nd = (NdArray v)
     -- Header string without length
     header = 
       "{'descr': '"<>   getNumpyDType nd  <> "', " <>
@@ -44,7 +44,7 @@ saveNpy path nd = withBinaryFile path WriteMode $ \h -> do
   hPutChar h '\n'
   -- Put vector body
   --unsafeWithPtr a $ \ptr -> hPutBuf h ptr (size nd * sizeOf (undefined :: (NumpyType a)=>a))
-  unsafeWith v (\ptr -> hPutBuf h ptr (size v * sizeOf (undefined :: Int)))
+  unsafeWith v (\ptr -> hPutBuf h ptr (3 * sizeOf (undefined :: Int)))
 
 saveNpz = undefined
 
@@ -53,4 +53,4 @@ loadNpy = undefined
 loadNpz = undefined
 
 main :: IO ()
-main = do saveNpy "./testout/idk.npy" (NdArray (fromList [1,2,3]))
+main = do saveNpy "./testout/idk.npy" (NdArray (fromList [1,2,3 :: Int]))
