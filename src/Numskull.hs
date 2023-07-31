b7f05caaeb29bb044e33ac1d5439545b336dedc5
Rowan <rowan.m.mather@gmail.com>
2023-07-31 18:02:24 +0100

    broadcasting

--- src/Numskull.hs
+++ src/Numskull.hs
@@ -12,6 +12,8 @@ import qualified DType
 import DType (DType)
 import MatrixForm
 import Serialisation
+import Indexing
+import Typing
 
 import qualified Data.Vector.Storable as V
 import Data.Vector.Storable (Vector)
@@ -23,26 +25,6 @@ import Data.List (sort, elemIndex)
 -- $setup
 -- >>> import Numskull as N
 -- >>> import qualified Vector
-
-
--- * Typing Shorthand 
--- | typeOf synonym.
-ty :: Typeable a => a -> TypeRep a
-ty = typeOf
-
--- | eqTypeRep synonym, returning Just HRefl in the case of type equality.
--- >>> case True =@= False of
--- >>>  Just HRefl -> putStrLn "Two Booleans will match"
--- >>>  Nothing    -> putStrLn "Mismatching types"
--- Two Booleans will match
-(=@=) :: (Typeable a, Typeable b) => a -> b -> Maybe (a :~~: b)
-(=@=) v u = eqTypeRep (ty v) (ty u)
-
--- Helper asserting a type
-(<-@) ::Typeable a => a -> TypeRep b -> b
-(<-@) val t = case eqTypeRep t (ty val) of
-  Just HRefl -> val
-  _ -> error "Mismatching type."
 
 instance Eq NdArray where
   -- | Arrays are equal if their elements and shape exactly match.
@@ -163,135 +145,6 @@ zeros _ s = NdArray s zerovec
   where
     ident = (DType.addId :: DType a => a)
     zerovec = (V.replicate (size s) ident) :: DType a => Vector a
-
-
--- * Indexing & Slicing
-{- | Arrays are stored as vectors with a shape. Since vectors only have one dimension,
-we convert between the vector index, i, and multi-dimension index, [x,y,z,...], using the 
-shape of the array, [sx,sy,sz,...], as follows: 
-  
-  i = x + y*sx + z*sx*sy + ...
-  
-  x = i/(1) % sx
-  y = i/(sx) % sy
-  z = i/(sx*sy) % sz 
-  ...
-
--}
-generateIndicies :: [Integer] -> [[Integer]]
-generateIndicies = foldr (\x xs -> [ (i:t) | i <- [0..(x-1)], t <- xs]) [[]]
--- foldr (\x xs -> [ (i:t) | i <- [0..x], t <- xs]) [[]] [2,3,2]
-
-mapIndicies :: [Integer] -> (M.Map Int [Integer], M.Map [Integer] Int)
-mapIndicies sh = (M.fromList oneDkey, M.fromList twoDkey)
-  where 
-    twoDinds = generateIndicies sh
-    oneDkey = zip [0..] twoDinds
-    twoDkey = zip twoDinds [0..]
-
--- Unsafe: Indexes the vector with the multi-index using a mapping 
-vecInd :: forall a . DType a => M.Map [Integer] Int -> Vector a -> [Integer] -> a
-vecInd mapp v i = v V.! (mapp M.! i)
---vecInd mapp v i = case v =@= (undefined :: Vector a) of
---  Just HRefl -> v V.! (mapp M.! i)
-
--- | Converts a shape and multi-index to a 1D index.
-collapseInd :: [Integer] -> [Integer] -> Integer
-collapseInd sh indicies = collapseRun (reverse$sh) (reverse$indicies) 1
-
--- Helper for collapseInd
-collapseRun :: [Integer] -> [Integer] -> Integer -> Integer
-collapseRun _ [] _ = 0
-collapseRun [] _ _ = 0
-collapseRun (s:ss) (x:xs) runSize = x*runSize + collapseRun ss xs (s*runSize)
-
--- | Converts a shape and 1D index to a multi-index.
-expandInd :: [Integer] -> Integer -> [Integer]
-expandInd sh i = reverse $ expandRun (reverse$sh) i 1
-
--- Helper for expandInd
-expandRun :: [Integer] -> Integer -> Integer -> [Integer]
-expandRun [] _ _ = []
-expandRun (s:ss) i runSize = x : expandRun ss i (s*runSize)
-  where x = (i `div` runSize) `mod` s
-
--- | Converts the multi-index for one shape to another
-map1DIndex :: [Integer] -> [Integer] -> Integer -> Integer
-map1DIndex s r i = collapseInd r (expandInd s i)
-
--- | Checks an index does not exceed the shape
-validIndex :: NdArray -> [Integer] -> Bool
-validIndex (NdArray s _) i = (length i == length s) && (and $ zipWith lessAbs i s)
-  where lessAbs x y = (0 <= x && x < y) || (0 < -x && -x <= y)
-
-{- | Takes a multi-dimensional index and returns the value in the NdArray at that position.
-Indicies can be negative, where -1 is the row in that dimension.
-If an index exceeds the size of its dimension, a value will still be returned, the identity
-value for the array e.g. 0. To avoid this use !?.
--} 
--- >>> m = fromListFlat [2,4,8 :: Int]
--- >>> m #! [1] :: Int
--- 4
--- >>> m #! [50] :: Int
--- 0
-(#!) :: DType a => NdArray -> [Integer] -> a
-(NdArray s v) #! i = case (NdArray s v) !? i of
-  Just val -> val
-  Nothing -> DType.addId :: DType a => a
-
-{- | The safer version of #! which returns Nothing if an index exceeds the shape bounds. -}
--- >>> m = fromListFlat [2,4,8 :: Int]
--- >>> m !? [1] :: Maybe Int
--- Just 4
--- >>> m !? [50] :: Maybe Int
--- Nothing
-(!?) :: forall a . DType a => NdArray -> [Integer] -> Maybe a
-(NdArray s v) !? i =
-  let 
-    -- Converts any negative indicies to their equivalent positives
-    positives = zipWith (\x y -> if y < 0 then x+y else y) s i
-    flatInd = fromIntegral $ collapseInd s positives :: Int
-  in
-    -- The type comparison should always hold
-    if validIndex (NdArray s v) i then
-      case ty v `eqTypeRep` typeRep @(Vector a) of
-        Just HRefl -> Just (v V.! flatInd) :: Maybe a -- Indexing the vector
-        Nothing -> Nothing
-    else Nothing
-
-slice :: [(Integer, Integer)] -> NdArray -> NdArray
-slice ss (NdArray sh v) = sliceWithMap m 0 ss (NdArray sh v)
-  where (m,_) = mapIndicies sh
-
-(!/) :: NdArray -> [(Integer, Integer)] -> NdArray
-(!/) nd ss = slice ss nd
-
--- helpser
-sliceWithMap :: M.Map Int [Integer] -> Int -> [(Integer, Integer)] -> NdArray -> NdArray
-sliceWithMap _ _ [] nd = nd
-sliceWithMap _ d _ (NdArray sh v) | d >= length sh = (NdArray sh v)
-sliceWithMap m d (s : ss) (NdArray sh v) = sliceWithMap m (d+1) ss $ 
-  sliceDim s d m (NdArray sh v)
-
--- inclusive
-sliceDim :: (Integer, Integer) -> Int -> M.Map Int [Integer] -> NdArray -> NdArray
-sliceDim (x,y) d m (NdArray s v) = 
-  if d >= length s then error "Given dimension does not exist in array."
-  else NdArray 
-    (if y < x then [] else shrinkNth d (y-x+1) s)
-    (V.ifilter 
-      (\i _ ->
-        let dimInd = (m M.! i) !! d
-        in x <= dimInd && dimInd <= y) 
-      v
-    )
-
--- https://stackoverflow.com/questions/5852722/replace-individual-list-elements-in-haskell
-shrinkNth :: Ord a => Int -> a -> [a] -> [a]
-shrinkNth _ _ [] = []
-shrinkNth n newVal (x:xs)
-  | n == 0 = if newVal < x then newVal:xs else x:xs
-  | otherwise = x:shrinkNth (n-1) newVal xs
 
 
 -- * Pointwise Functions  -- 
@@ -474,6 +327,57 @@ padShape r (NdArray s v) =
     if smallerShape s r 
     then NdArray r (V.unsafeUpdate_ nullVec newIndices v)
     else error "Cannot map to a smaller shape."
+
+--broadcast :: forall a . DType a => (NdArray, NdArray) -> Maybe (Vector a, Vector a)
+broadcast :: (NdArray, NdArray) -> Maybe (NdArray, NdArray)
+broadcast ((NdArray s v), (NdArray r u)) = 
+  let 
+    (s',v',r',u') = broadcastDimensions s v r u
+    newshape = sequenceA $ zipWith (\x y -> if x == y || x == 1 || y == 1 
+      then Just (max x y) else Nothing) s' r'
+  in
+    case newshape of
+      Nothing -> Nothing
+      Just ns -> Just (
+        NdArray ns $ padRepeats ns m s' v', --NdArray ns $ 
+        NdArray ns $ padRepeats ns m r' u') -- NdArray ns $ 
+        where m = (fst $ mapIndicies ns)
+
+-- makes the number of dimensions correct but not the size of the later 1s 
+broadcastDimensions :: (DType a, DType b) => 
+  [Integer] -> Vector a -> [Integer] -> Vector b -> 
+    ([Integer], Vector a, [Integer], Vector b)
+broadcastDimensions s v r u 
+  | sl == rl = (s,v,
+                r,u)
+  |  sl > rl = (s,v,
+                sdiff ++ r,
+                V.concat $ replicate (fromIntegral $ product $ sdiff) u)
+  |  sl < rl = (rdiff ++ s,
+                V.concat $ replicate (fromIntegral $ product $ rdiff) v,
+                r,u)
+  where
+    sl = length s
+    rl = length r
+    diff = Prelude.abs (sl - rl)
+    sdiff = take diff s
+    rdiff = take diff r
+
+padRepeats :: DType a => 
+  [Integer] -> M.Map Int [Integer] -> [Integer] -> Vector a -> Vector a
+padRepeats newshape oneDmap s v =
+  let (_, multiMap) = mapIndicies s
+  in V.generate (fromIntegral $ product newshape) (\i ->
+    let
+        multiI = oneDmap M.! i -- equivalent multi-index
+        multiWrap = zipWith mod multiI s -- wrap the index over dimensions of size 1
+        flatWrap = multiMap M.! multiWrap -- collapse the index over the vector
+    in v V.! flatWrap) 
+
+-- Concatenate a list of tensors into a single tensor. All input tensors must have the#
+-- same shape, except for the dimension size of the axis to concatenate on.
+concat :: [NdArray] -> Integer -> Maybe NdArray
+concat = undefined
 
 -- * Matrix Operations
 
---
+++ src/Typing.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Typing where
+
+import Type.Reflection
+
+-- * Typing Shorthand 
+-- | typeOf synonym.
+ty :: Typeable a => a -> TypeRep a
+ty = typeOf
+
+-- | eqTypeRep synonym, returning Just HRefl in the case of type equality.
+-- >>> case True =@= False of
+-- >>>  Just HRefl -> putStrLn "Two Booleans will match"
+-- >>>  Nothing    -> putStrLn "Mismatching types"
+-- Two Booleans will match
+(=@=) :: (Typeable a, Typeable b) => a -> b -> Maybe (a :~~: b)
+(=@=) v u = eqTypeRep (ty v) (ty u)
+
+-- Helper asserting a type
+(<-@) ::Typeable a => a -> TypeRep b -> b
+(<-@) val t = case eqTypeRep t (ty val) of
+  Just HRefl -> val
+  _ -> error "Mismatching type."
