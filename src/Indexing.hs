d6928321faeb72c89184b11fd80b5ad4b7d75dbc
Rowan <rowan.m.mather@gmail.com>
2023-08-10 17:08:56 +0100

    Numskull 2.0
    starting stride redesign
    
    updating lots of functions
    
    updating lots of functions
    
    pretty printing & broadcast config
    
    pointwise stride & some matmul work
    
    transposition
    
    some determinants fixing
    
    more determinant ops
    
    concatenation
    
    bug fixing
    
    slices & compiles
    
    single indexing
    
    s

--- src/Indexing.hs
+++ src/Indexing.hs
@@ -1,26 +1,47 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 module Indexing where
+
 
 import Control.Exception
 import qualified Data.Vector.Storable as V
 import Data.Vector.Storable (Vector)
 import qualified Data.Map as M
 import Type.Reflection
-import Text.ParserCombinators.Parsec
 
 import NdArray
 import Typing
 import qualified DType
 import DType (DType)
 import NdArrayException
-import QuasiSlice
-import QuasiSlice.Quote
 
+preciseDiv x y = fromIntegral @Int @Float x / fromIntegral @Int @Float y
+
+-- Takes the shape assuming you want to move directly between dimensions.
+defStride :: Vector Int -> Vector Int
+defStride sh = V.scanr' (*) 1 $ V.drop 1 sh
+
+stride :: NdArray -> NdArray
+stride (NdArray sh st v) = 
+  let
+    -- shape
+    dimAcc = V.scanr' (*) 1 sh
+    --newshape = V.map (i -> ((dim' V.!(i+1)) * (sh V.!i -1) +1) / st V.! i) (enumFromN 0 (V.length sh))  
+    newshape =
+      V.map (\i ->
+          ceiling $ preciseDiv (1 + (dimAcc V.! (i+1)) * (sh V.!i -1)) (st V.! i) :: Int)
+        (V.enumFromN 0 (V.length sh))
+    --V.zipWith (\d s -> ceiling (preciseDiv d s) :: Int) [0..] st
+    -- stride
+    singleStride = defStride newshape
+    -- vector
+    grab i = V.and $ V.zipWith (\t h -> i `mod` t < h) st (V.drop 1 dimAcc)
+    newV = V.force (V.ifilter (\i _ -> grab i) v)
+  in
+    if V.all (==1) st then (NdArray sh st v)
+    else NdArray newshape singleStride newV
 
 {- | Arrays are stored as vectors with a shape. Since vectors only have one dimension,
 we convert between the vector index, i, and multi-dimension index, [x,y,z,...], using the 
@@ -37,14 +58,14 @@ shape of the array, [sx,sy,sz,...], as follows:
 -- * INDEXING
 
 -- | Generates the list of all multi-dimensional indices for a given shape
-generateIndices :: [Integer] -> [[Integer]]
+generateIndices :: [Int] -> [[Int]]
 generateIndices = foldr (\x xs -> [ i:t | i <- [0..(x-1)], t <- xs]) [[]]
 
 {- | Generates two maps to convert between the single dimension index of the 
 underlying vector and the multi-dimensional index of the NdArray and back, 
 given the NdArray shape.
 -}
-mapIndices :: [Integer] -> (M.Map Int [Integer], M.Map [Integer] Int)
+mapIndices :: [Int] -> (M.Map Int [Int], M.Map [Int] Int)
 mapIndices sh = (M.fromList oneDkey, M.fromList twoDkey)
   where 
     twoDinds = generateIndices sh
@@ -52,9 +73,10 @@ mapIndices sh = (M.fromList oneDkey, M.fromList twoDkey)
     twoDkey = zip twoDinds [0..]
 
 -- Indexes a vector with an NdArray multi-index using a mapping (unsafe).
-vecInd :: forall a . DType a => M.Map [Integer] Int -> Vector a -> [Integer] -> a
-vecInd mapp v i = v V.! (mapp M.! i)
+--vecInd :: forall a . DType a => M.Map [Int] Int -> Vector a -> [Int] -> a-
+--vecInd mapp v i = v V.! (mapp M.! i)
 
+{-
 -- | Converts a shape and multi-index to a 1D index.
 collapseInd :: [Integer] -> [Integer] -> Integer
 collapseInd sh indices = collapseRun (reverse sh) (reverse indices) 1
@@ -74,15 +96,43 @@ expandRun :: [Integer] -> Integer -> Integer -> [Integer]
 expandRun [] _ _ = []
 expandRun (s:ss) i runSize = x : expandRun ss i (s*runSize)
   where x = (i `div` runSize) `mod` s
+-}
+vGet :: DType a => Vector a -> Vector Int -> [Int] -> a
+vGet v t is = v V.! (collapseInd t $ V.fromList is)
+
+collapseInd :: Vector Int -> Vector Int -> Int
+collapseInd st ind = V.sum $ V.zipWith (*) st ind
+
+expandInd :: Vector Int -> Int -> Vector Int
+expandInd st ind = 
+  let st' = V.toList st 
+  in V.fromList $ expandRun st' ind
+
+expandRun :: [Int] -> Int -> [Int]
+expandRun [] _ = []
+expandRun (s:sts) x =
+  if s == 0 then (0 : expandRun sts x)
+  else x `div` s : expandRun sts (x `mod` s)
+  {-
+  let st' = V.map (/= 0) st
+  V.zipwith (/) (V.drop 1 $ V.scanl mod ind st) st
+
+scanl :: (b -> a -> b) -> b -> [a] -> [b]
+scanl f z xs = foldr go (const []) xs z
+  where
+    go x continue acc = let next = f acc x in next : continue next
+-}
 
 -- | Converts the multi-index for one shape to another
-map1DIndex :: [Integer] -> [Integer] -> Integer -> Integer
-map1DIndex s r i = collapseInd r (expandInd s i)
+map1DIndex :: Vector Int -> Vector Int -> Int -> Int
+map1DIndex t d i = collapseInd d (expandInd t i)
 
 -- | Checks an index does not exceed the shape.
-validIndex :: NdArray -> [Integer] -> Bool
-validIndex (NdArray s _) i = (length i == length s) && and (zipWith lessAbs i s)
-  where lessAbs x y = (0 <= x && x < y) || (0 < -x && -x <= y)
+validIndex :: NdArray -> [Int] -> Bool
+validIndex (NdArray _ sh _) i = (length i == length s) && and (zipWith lessAbs i s)
+  where
+    s = V.toList sh 
+    lessAbs x y = (0 <= x && x < y) || (0 < -x && -x <= y)
 
 {- | Takes a multi-dimensional index and returns the value in the NdArray at that position.
 Indices can be negative, where -1 is the row in that dimension.
@@ -94,8 +144,8 @@ value for the array e.g. 0. To avoid this use !?.
 -- 4
 -- >>> m #! [50] :: Int
 -- 0
-(#!) :: DType a => NdArray -> [Integer] -> a
-(NdArray s v) #! i = case NdArray s v #? i of
+(#!) :: DType a => NdArray -> [Int] -> a
+(NdArray st sh v) #! i = case NdArray sh sh v !? i of
   Just val -> val
   Nothing -> DType.addId :: DType a => a
 
@@ -105,70 +155,60 @@ value for the array e.g. 0. To avoid this use !?.
 -- Just 4
 -- >>> m !? [50] :: Maybe Int
 -- Nothing
-(#?) :: forall a . DType a => NdArray -> [Integer] -> Maybe a
-(NdArray s v) #? i =
+(!?) :: forall a . DType a => NdArray -> [Int] -> Maybe a
+(NdArray st sh v) !? i =
   let
     -- Converts any negative indices to their equivalent positives
-    positives = zipWith positiveInd s i
-    flatInd = fromIntegral $ collapseInd s positives :: Int
+    positives = V.zipWith positiveInd sh (V.fromList i)
+    flatInd = fromIntegral $ collapseInd st positives :: Int
   in
     -- The type comparison should always hold
-    if validIndex (NdArray s v) i then
+    if validIndex (NdArray st sh v) i then
       case ty v `eqTypeRep` typeRep @(Vector a) of
         Just HRefl -> Just (v V.! flatInd) :: Maybe a -- Indexing the vector
         Nothing -> Nothing
     else Nothing
 
+-- * SLICING
+{-
+-- | Type which allows you to provide only a single index or a range of indices.
+data IndexRange = I Integer | R Integer Integer deriving (Show, Eq)
+
+-- | Integrated indexing and slicing. For each dimension you can provide either a single value
+-- or a range of values where a slice will be taken.
+(#!+) :: NdArray -> [IndexRange] -> NdArray
+(#!+) (NdArray sh v) irs = sliceWithMap m 0 (map forceRange irs) (NdArray sh v)
+  where (m,_) = mapIndices sh
+
+-- Converts an IndexRange to a range of indices in the standard pair form.
+forceRange :: IndexRange -> (Integer, Integer)
+forceRange (I i) = (i,i)
+forceRange (R s t) = (s,t)
+-}
 -- Converts negative indices to their positive equivalents, counting back
 -- from the end of the array (i.e. -1 is the last element).
 positiveInd :: (Ord a, Num a) => a -> a -> a
 positiveInd s i = if i < 0 then s+i else i
 
--- * SLICING
-
-positiveRanges :: [Integer] -> [(Integer, Integer)] -> [(Integer, Integer)]
-positiveRanges = zipWith (\s (x,y) -> (positiveInd s x, if y < 0 then s+y else y-1))
-
--- Converts an IndexRange to a range of indices in the standard pair form.
-forceRange :: Integer -> IndexRange -> (Integer, Integer)
-forceRange sh (I i) = (positiveInd sh i, positiveInd sh i)
-forceRange sh (R s t) = (positiveInd sh s, if t < 0 then positiveInd sh t else t-1)
-
--- | The concise operator for slicing. Instead of providing an IndexRange, 
--- You may QuasiQuote a NumPy-like index e.g. myArray /! [q|5,2:6,:3|].
--- Unspecified values in ranges denote the start/end.
-(/!) :: NdArray -> QuasiSlice -> NdArray
-(/!) nd sl = nd #!+ (evalSlice sl)
-
--- Integrated indexing and slicing. For each dimension you can provide either a single value
--- or a range of values where a slice will be taken.
-(#!+) :: NdArray -> [IndexRange] -> NdArray
-(#!+) (NdArray s v) irs = slice (zipWith forceRange s irs) (NdArray s v)
-
 {- | Takes a series of ranges corresponding to each dimension in the array and returns
 the sub-array. Indices are inclusive and can be negative. -}
-slice :: [(Integer, Integer)] -> NdArray -> NdArray
-slice sl (NdArray s v) =
-  let
-    pad = sl ++ replicate (length s - length sl) (0,-1)
-    sl' = zipWith (\(x,y) sk -> (positiveInd sk x, positiveInd sk y)) pad s 
-    inds = sequence $ map (\(x, y) -> [x..y]) sl'
-    flatinds = V.fromList $ map (fromInteger @Int . collapseInd s) inds
-    newshape = map (\(x,y) -> y-x+1) sl'
-  in NdArray newshape $ V.map (v V.!) flatinds
-
-{-
-slicing with maps
---slice :: [(Integer, Integer)] -> NdArray -> NdArray
+slice :: [(Int, Int)] -> NdArray -> NdArray
+slice sl (NdArray sh st v) =
+  let -- todo: -ve indices
+    ranges = zipWith (\(i,j) t -> [t*i, t*(i+1) .. t*j]) sl (V.toList st)
+    indices = V.fromList $ (map sum . sequence) ranges
+    sh' = V.fromList $ map (\(i,j) -> j-i+1) sl
+    v' = V.map (v V.!) indices
+  in 
+     NdArray sh' (defStride sh') v'
 --slice ss (NdArray sh v) = sliceWithMap m 0 ss (NdArray sh v)
 --  where (m,_) = mapIndices sh
+-- https://rosettacode.org/wiki/Cartesian_product_of_two_or_more_lists#Haskell
 
---(#!+) (NdArray sh v) irs = sliceWithMap m 0 (map forceRange irs) (NdArray sh v)
---  where (m,_) = mapIndices sh
-
--- Equivalent slicing operator.
---(!/) :: NdArray -> [(Integer, Integer)] -> NdArray
---(!/) nd ss = slice ss nd
+{-
+-- | Equivalent slicing operator.
+(!/) :: NdArray -> [(Integer, Integer)] -> NdArray
+(!/) nd ss = slice ss nd
 
 -- Takes a slice on an NdArray given the mapping from the vector index to NdArray index.
 -- Iterates through each dimension of the slice one at a time. 
@@ -178,20 +218,21 @@ sliceWithMap _ d _ (NdArray sh v) | d >= length sh = NdArray sh v
 sliceWithMap m d (s : ss) (NdArray sh v) = sliceWithMap m (d+1) ss $ 
   sliceDim s d m (NdArray sh v)
 
--- Takes a slice of an NdArray at a particular dimension, no -ve indices.
+-- Takes a slice of an NdArray at a particular dimension.
 sliceDim :: (Integer, Integer) -> Int -> M.Map Int [Integer] -> NdArray -> NdArray
 sliceDim (x,y) d m (NdArray sh v) = 
   if d >= length sh then throw (ExceededShape (fromIntegral d) sh)
   else NdArray
-    (if y < x then [] else shrinkNth d (y-x+1) sh)
+    (if y' < x' then [] else shrinkNth d (y'-x'+1) sh)
     (V.ifilter
       (\i _ ->
         let dimInd = (m M.! i) !! d
-        in x <= dimInd && dimInd <= y) 
+        in x' <= dimInd && dimInd <= y') 
       v
     )
   where
     dimSize = sh !! d 
+    (x', y') = (positiveInd dimSize x, positiveInd dimSize y)
 
 -- Replaces the nth value of an array if the newValue is smaller.
 -- https://stackoverflow.com/questions/5852722/replace-individual-list-elements-in-haskell
@@ -200,4 +241,4 @@ shrinkNth _ _ [] = []
 shrinkNth n newVal (x:xs)
   | n == 0 = if newVal < x then newVal:xs else x:xs
   | otherwise = x:shrinkNth (n-1) newVal xs
--}
+  -}
--- src/NdArray.hs
+++ src/NdArray.hs
@@ -4,18 +4,15 @@ module NdArray where
 
 import DType
 import Data.Vector.Storable
-import Control.DeepSeq (NFData (rnf), deepseq)
 
 -- * NdArray
 -- | The core of this module. NdArrays can be of any DType a and size/shape (list of dimensions)
 -- These are hidden by the type. 
 data NdArray where
-  NdArray :: DType a => [Integer] -> Vector a -> NdArray
+  -- shape stride elements
+  NdArray :: DType a => Vector Int -> Vector Int -> Vector a -> NdArray
 
 -- | By default arrays are printed flat with the shape as metadata. 
 -- For a tidier representation, use printArray.
 instance Show NdArray where
-  show (NdArray s v) = "{elements: " <> show v <> ", shape: " <> show s <> "}"
-
-instance NFData NdArray where
-  rnf (NdArray s v) = s `deepseq` v `deepseq` ()
+  show (NdArray sh st v) = "{elements: " <> show v <> ", shape: " <> show sh <> ", stride: " <> show st <> "}"
