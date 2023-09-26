5b22240de0a73e26045c8f79224986b3d679e8ed
Aiken Cairncross <acairncross@gmail.com>
2023-09-27 00:10:59 +0100

    Add NFData instance

--- src/NdArray.hs
+++ src/NdArray.hs
@@ -4,6 +4,7 @@ module NdArray where
 
 import DType
 import Data.Vector.Storable
+import Control.DeepSeq (NFData (rnf), deepseq)
 
 -- * NdArray
 -- | The core of this module. NdArrays can be of any DType a and size/shape (list of dimensions)
@@ -15,3 +16,6 @@ data NdArray where
 -- For a tidier representation, use printArray.
 instance Show NdArray where
   show (NdArray s v) = "{elements: " <> show v <> ", shape: " <> show s <> "}"
+
+instance NFData NdArray where
+  rnf (NdArray s v) = s `deepseq` v `deepseq` ()
