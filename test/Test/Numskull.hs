0c92e566f01eabc2088b7e45ffdb16109ae12c57
Aiken Cairncross <acairncross@gmail.com>
2023-10-10 13:04:25 +0100

    Add matmul test

--- test/Test/Numskull.hs
+++ test/Test/Numskull.hs
@@ -11,9 +11,14 @@ import Numskull as N
 
 spec :: Spec
 spec = do
-    describe "NdArray equality" $
-        it "works" $
-           N.fromList [3] [1,2,3::Int] == N.fromList [3] [1,2,3::Int]
+  describe "NdArray equality" $
+    it "works" $
+      N.fromList [3] [1,2,3::Int] == N.fromList [3] [1,2,3::Int]
+
+  describe "NdArray matmul" $
+    it "2x3 times 3x4" $
+      (N.fromList [2,3] [1..2*3 :: Int] `matMul` N.fromList [3,4] [1..3*4 :: Int]) `shouldBe`
+        N.fromList [2,4] [38, 44, 50, 56, 83, 98, 113, 128 :: Int]
 
 {-
     describe "padShape" $ do
@@ -23,11 +28,3 @@ spec = do
             in
             padShape (N.fromList [n] content) [n + extra] `shouldBe` N.fromList [n + extra] (content <> replicate (fromInteger extra) (0 :: Int))
 -}
---cabal test --test-show-details=streaming
--- ghci -isrc -itest test/Test/Numskull.hs
---     ghci> hspec spec
-
-
--- https://hackage.haskell.org/package/hspec-2.11.3/docs/Test-Hspec.html#v:example
-
--- https://hspec.github.io/
